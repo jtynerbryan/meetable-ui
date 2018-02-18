911c229180a2f6acd086d7b15a4957981517ce4e
David Gavazzi <dgavazzi@users.noreply.github.com>
2018-02-18 14:52:00 -0500

    Update LandingPage.js
    Another test for typing animation speed.

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -73,8 +73,8 @@ class LandingPage extends React.Component {
           <p>AI</p>
           <Typed
             className="typed-text"
-            strings={['for meeting IRL', 'for meeting up with friends', 'for planning first dates', 'for making work coffees happen', 'for meeting IRL']}
-            typeSpeed={90}
+            strings={['for meeting IRL', 'for meeting up with friends', 'for planning first dates', 'for making work coffees happen']}
+            typeSpeed={120}
             loop
           />
         </div>
